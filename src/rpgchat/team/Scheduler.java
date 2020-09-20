36d0ed870cf2cfb413d9af9fa669afe1e40493e8
alexandrage <alexandrage@mail.ru>
2020-09-20 20:45:10 +0300

    up

--- src/rpgchat/team/Scheduler.java
+++ src/rpgchat/team/Scheduler.java
@@ -12,7 +12,9 @@ import rpgchat.Main;
 import rpgchat.packet.WrapperPlayServerScoreboardTeam;
 
 public class Scheduler extends BukkitRunnable {
-	Main plugin;
+	private Main plugin;
+	private static String version;
+	private static boolean isNew;
 
 	public Scheduler(Main team) {
 		this.plugin = team;
@@ -27,29 +29,32 @@ public class Scheduler extends BukkitRunnable {
 	}
 
 	private void setTeam(Player player, Collection<? extends Player> players) {
-		WrapperPlayServerScoreboardTeam remove = sendTeam(player, 1);
-		WrapperPlayServerScoreboardTeam created = sendTeam(player, 0);
-		for (Player p : players) {
-			if (this.plugin.getConfig().getBoolean("tabworld")) {
-				try {
-					if (player.getWorld() == p.getWorld()) {
-						player.showPlayer(this.plugin, p);
-					} else {
-						player.hidePlayer(this.plugin, p);
+		if (isNew) {
+			WrapperPlayServerScoreboardTeam remove = sendTeam(player, 1);
+			WrapperPlayServerScoreboardTeam created = sendTeam(player, 0);
+			for (Player p : players) {
+				if (this.plugin.getConfig().getBoolean("tabworld")) {
+					try {
+						if (player.getWorld() == p.getWorld()) {
+							player.showPlayer(this.plugin, p);
+						} else {
+							player.hidePlayer(this.plugin, p);
+						}
+					} catch (Exception e) {
 					}
-				} catch (Exception e) {
 				}
+				remove.sendPacket(p);
+				created.sendPacket(p);
 			}
-			remove.sendPacket(p);
-			created.sendPacket(p);
 		}
-
 	}
 
 	private WrapperPlayServerScoreboardTeam sendTeam(Player player, int i) {
 		WrapperPlayServerScoreboardTeam team = new WrapperPlayServerScoreboardTeam();
 		BaseComponent[] c = TextComponent.fromLegacyText(trim(this.plugin.chat.getPlayerPrefix(player)));
-		team.setColor(ChatColor.WHITE);
+		String pref = trim(this.plugin.chat.getPlayerPrefix(player));
+		BaseComponent[] temp = TextComponent.fromLegacyText(pref);
+		team.setColor(ChatColor.getByChar(temp[temp.length - 1].getColor().toString().substring(1)));
 		team.setName(trimt(c[c.length - 1].getColor().toString().substring(1) + player.getName()));
 		team.setDisplayName(WrappedChatComponent.fromText(player.getName()));
 		team.setMode(i);
@@ -73,5 +78,16 @@ public class Scheduler extends BukkitRunnable {
 		if (color.length() > 16)
 			return color.substring(0, 16);
 		return color;
+	}
+
+	static {
+		version = Bukkit.getServer().getClass().getName().split("\\.")[3];
+		try {
+			Class.forName("net.minecraft.server." + version + ".EnumChatFormat");
+			Class.forName("net.minecraft.server." + version + ".EnumChatFormat");
+			isNew = true;
+		} catch (ClassNotFoundException e1) {
+			isNew = false;
+		}
 	}
 }
